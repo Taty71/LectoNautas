09eb6c516aeb1035fdb3062354f1214ec0730d08
Taty71 <cristinbmaia@gmail.com>
2026-04-08 00:29:25 -0300

    Lowercase todo

--- routes/lecturaRoutes.js
+++ routes/lecturaRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Lectura = require('../models/Lectura');
+const Lectura = require('../models/lectura.js');
 
 router.post('/evaluar', async (req, res) => {
     try {
